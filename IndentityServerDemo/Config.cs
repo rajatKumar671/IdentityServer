8163e5686cecccd9b1818b33ecbe357cd49f5a8e
Rajat Kumar <41984186+rajatKumar671@users.noreply.github.com>
2019-03-18 10:53:58 +0530

    Initial Commit
    Initial Commit

---
+++ IndentityServerDemo/Config.cs
@@ -0,0 +1,79 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace IndentityServerDemo
+{
+    public class Config
+    {
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+    {
+        // other clients omitted...
+
+        // OpenID Connect implicit flow client (MVC)
+        new Client
+        {
+            ClientId = "mvc",
+            ClientName = "MVC Client",
+            AllowedGrantTypes = GrantTypes.Implicit,
+            RequireConsent=false,
+
+            // where to redirect to after login
+            RedirectUris = { "http://localhost:51893/signin-oidc" },
+            
+            // where to redirect to after logout
+            PostLogoutRedirectUris = { "http://localhost:51893/signout-callback-oidc" },
+
+            AllowedScopes = new List<string>
+            {
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile,
+            },
+            AllowAccessTokensViaBrowser=true,
+        }
+    };
+        }
+
+        public static IEnumerable<IdentityResource> IdentityResources = new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+        };
+
+        public static List<TestUser> GetUsers = new List<TestUser>
+    {
+        new TestUser
+        {
+            SubjectId = "1",
+            Username = "alice",
+            Password = "password",
+
+            Claims = new []
+            {
+                new Claim("name", "Alice"),
+                new Claim("website", "https://alice.com")
+            }
+        },
+        new TestUser
+        {
+            SubjectId = "2",
+            Username = "bob",
+            Password = "password",
+
+            Claims = new []
+            {
+                new Claim("name", "Bob"),
+                new Claim("website", "https://bob.com")
+            }
+        }
+    };
+    }
+}
+
---
+++ mvc/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using mvc.Models;
+
+namespace mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string cookieValueFromReq = Request.Cookies["idsrv"];
+            if (cookieValueFromReq == null)
+            {
+                return login();
+            }
+            return View();
+        }
+
+        public IActionResult login()
+        {   
+            return Challenge(new AuthenticationProperties
+            {
+                RedirectUri = "/Home/Index"
+            }, "oidc");
+        }
+
+        public IActionResult logout()
+        {
+            return SignOut(new AuthenticationProperties
+            {
+                RedirectUri = "/Home/Index"
+            }, "Cookies", "oidc");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
